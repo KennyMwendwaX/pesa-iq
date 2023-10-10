4b7628faaa71dfd3b205df4eb195593ee4d9e11f
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-10 15:22:27 +0300

    npx shadcn-ui@latest add avatar

---
+++ components/Navbar.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { MagnifyingGlassIcon, PersonIcon } from "@radix-ui/react-icons";
+
+export default function Navbar() {
+  const pathname = usePathname();
+
+  return (
+    <>
+      <nav className="bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-600">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-2">
+          <div className="flex items-center space-x-8">
+            <Link href="/">
+              <span className="self-center text-xl font-semibold whitespace-nowrap text-slate-200">
+                TaskTracker
+              </span>
+            </Link>
+            <div className="space-x-3">
+              <Link href="/" className="text-gray-200">
+                Home
+              </Link>
+              <Link href="/" className="text-gray-200">
+                Link
+              </Link>
+            </div>
+          </div>
+          <div className="flex md:order-2 items-center space-x-3">
+            <div className="relative hidden md:block">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none">
+                <MagnifyingGlassIcon className="text-gray-400 w-5 h-5" />
+                <span className="sr-only">Search icon</span>
+              </div>
+              <input
+                type="text"
+                id="search-navbar"
+                className="block w-full outline-none p-2 pl-8 text-sm border rounded-lg bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                placeholder="Search..."
+              />
+            </div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="ghost"
+                  className="relative h-8 w-8 rounded-full">
+                  <Avatar className="h-8 w-8">
+                    {/* <AvatarImage src="/avatars/01.png" alt="@shadcn" /> */}
+                    <AvatarFallback>
+                      <PersonIcon className="h-5 w-5" />
+                    </AvatarFallback>
+                  </Avatar>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56" align="end" forceMount>
+                <DropdownMenuLabel className="font-normal">
+                  <div className="flex flex-col space-y-1">
+                    <p className="text-sm font-medium leading-none">shadcn</p>
+                    <p className="text-xs leading-none text-muted-foreground">
+                      m@example.com
+                    </p>
+                  </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    Profile
+                    <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    Billing
+                    <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    Settings
+                    <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>New Team</DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  Log out
+                  <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
