ad84a84d9df977e43ea5e61810e123e4cf43b89b
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-11 07:37:46 +0300

    npx shadcn-ui@latest add label

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 export default function Sidebar() {
   return (
     <>
-      <div>Sidebar</div>
+      <Sheet>
+        <SheetTrigger>Open</SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader>
+            <SheetTitle>Are you sure absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
     </>
   );
 }
