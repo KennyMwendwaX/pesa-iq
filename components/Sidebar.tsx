2bf69e186ce620f3537e8cda90f85c5e466ed0a4
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-12 14:56:50 +0300

    npm i react-icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,7 +13,12 @@ import {
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { HamburgerMenuIcon, DashboardIcon } from "@radix-ui/react-icons";
+import {
+  HamburgerMenuIcon,
+  DashboardIcon,
+  PersonIcon,
+} from "@radix-ui/react-icons";
+import { BarChart4Icon, GraduationCapIcon } from "lucide-react";
 
 export default function Sidebar() {
   return (
@@ -36,6 +41,30 @@ export default function Sidebar() {
             <div className="flex items-center space-x-4">
               <DashboardIcon className="w-5 h-5" />
               <span>Dashboard</span>
+            </div>
+            <div className="flex items-center space-x-4">
+              <PersonIcon className="w-5 h-5" />
+              <span>Profile</span>
+            </div>
+            <div className="flex items-center space-x-4">
+              <BarChart4Icon className="w-5 h-5" />
+              <span>Budget and Savings</span>
+            </div>
+            <div className="flex items-center space-x-4">
+              <BarChart4Icon className="w-5 h-5" />
+              <span>Analytics and Reports</span>
+            </div>
+            <div className="flex items-center space-x-4">
+              <BarChart4Icon className="w-5 h-5" />
+              <span>Financial Goals</span>
+            </div>
+            <div className="flex items-center space-x-4">
+              <GraduationCapIcon className="w-5 h-5" />
+              <span>Investment Center</span>
+            </div>
+            <div className="flex items-center space-x-4">
+              <GraduationCapIcon className="w-5 h-5" />
+              <span>Educational Resources</span>
             </div>
           </div>
           <SheetFooter>
