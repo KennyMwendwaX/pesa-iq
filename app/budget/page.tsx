757a75263360da101824ec37d992336aba092723
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-17 19:05:26 +0300

    Added a Navbarto the budget page

--- app/budget/page.tsx
+++ app/budget/page.tsx
@@ -1,6 +1,9 @@
+import Navbar2 from "@/components/Navbar2";
+
 export default function page() {
   return (
     <>
+      <Navbar2 />
       <div className="container mx-auto mt-4 px-12 pb-5 pt-12">page</div>
     </>
   );
