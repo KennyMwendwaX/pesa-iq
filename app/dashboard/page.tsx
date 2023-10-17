df32386ef8dc21ef5788800b5fc9587172db723a
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-17 19:04:49 +0300

    Added a container to the dashboard and budget pages

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,7 @@ export default function Dashboard() {
   return (
     <>
       <Navbar2 />
-      <div>Dashboard</div>
+      <div className="container mx-auto mt-4 px-12 pb-5 pt-12">Dashboard</div>
     </>
   );
 }
