303b8892d6175a4e9ffda545c5974f38b27a8cd5
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-18 21:40:44 +0300

    Imported the AddIncome modal to the income page

--- app/income/page.tsx
+++ app/income/page.tsx
@@ -1,7 +1,5 @@
 import Navbar2 from "@/components/Navbar2";
-import { Button } from "@/components/ui/button";
-import { HiPlus } from "react-icons/hi2";
-import { AiOutlinePlus } from "react-icons/ai";
+import AddIncomeModal from "@/components/AddIncomeModal";
 
 export default function Income() {
   return (
@@ -9,10 +7,7 @@ export default function Income() {
       <Navbar2 />
       <div className="container mx-auto mt-4 px-12 pb-5 pt-12">
         <div>Income</div>
-        <Button className="flex items-center space-x-2 rounded-3xl">
-          <AiOutlinePlus className="w-4 h-4 text-white" />
-          <span>Add Income</span>
-        </Button>
+        <AddIncomeModal />
       </div>
     </>
   );
