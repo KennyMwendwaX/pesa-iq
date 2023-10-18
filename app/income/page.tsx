e366c20b91b7a37f8972c22824d3902b7d184780
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-18 21:17:35 +0300

    Created /income/page.tsx

--- app/income/page.tsx
+++ app/income/page.tsx
@@ -1,11 +1,11 @@
 import Navbar2 from "@/components/Navbar2";
 
-export default function Budget() {
+export default function Income() {
   return (
     <>
       <Navbar2 />
       <div className="container mx-auto mt-4 px-12 pb-5 pt-12">
-        <div>Budget</div>
+        <div>Income</div>
       </div>
     </>
   );
