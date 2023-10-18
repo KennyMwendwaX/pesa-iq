0e8066bc662e931f3ae5da96063aa5f6984a7a12
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-18 21:19:02 +0300

    Added metadata /income/layout.tsx

--- app/income/layout.tsx
+++ app/income/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 
-// export const metadata: Metadata = {
-//   title: "Dashboard",
-//   description: "Dashboard page",
-// };
+export const metadata: Metadata = {
+  title: "Income",
+  description: "Income page",
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
