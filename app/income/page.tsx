f8ff51d68df9326c14cd2ad609a402e075884108
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-18 21:34:17 +0300

    npx shadcn-ui@latest add dialog

--- app/income/page.tsx
+++ app/income/page.tsx
@@ -1,4 +1,7 @@
 import Navbar2 from "@/components/Navbar2";
+import { Button } from "@/components/ui/button";
+import { HiPlus } from "react-icons/hi2";
+import { AiOutlinePlus } from "react-icons/ai";
 
 export default function Income() {
   return (
@@ -6,6 +9,10 @@ export default function Income() {
       <Navbar2 />
       <div className="container mx-auto mt-4 px-12 pb-5 pt-12">
         <div>Income</div>
+        <Button className="flex items-center space-x-2 rounded-3xl">
+          <AiOutlinePlus className="w-4 h-4 text-white" />
+          <span>Add Income</span>
+        </Button>
       </div>
     </>
   );
