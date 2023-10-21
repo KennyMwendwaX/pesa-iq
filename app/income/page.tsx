e7464af2fa0ec243f97e9fecf143b5fc41ec2af4
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-21 13:28:57 +0300

    Added Category icon for income card

--- app/income/page.tsx
+++ app/income/page.tsx
@@ -9,6 +9,8 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Progress } from "@/components/ui/progress";
+import { FaMoneyBillTrendUp } from "react-icons/fa6";
 
 export default function Income() {
   return (
@@ -18,26 +20,36 @@ export default function Income() {
         <div>Income</div>
         <AddIncomeModal />
         <div className="flex pt-3">
-          <Table className="w-[800px]">
-            <TableCaption>A list of your recent invoices.</TableCaption>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[100px]">Income</TableHead>
-                <TableHead>Amount</TableHead>
-                <TableHead>Progress</TableHead>
-                <TableHead>Date</TableHead>
-                <TableHead className="text-right">comment</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableCell className="font-medium">INV001</TableCell>
-                <TableCell>Paid</TableCell>
-                <TableCell>Credit Card</TableCell>
-                <TableCell className="text-right">$250.00</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
+          <div className="w-[800px]">
+            <Table>
+              <TableCaption>A list of your recent invoices.</TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[100px]">Income</TableHead>
+                  <TableHead>Amount</TableHead>
+                  <TableHead>Progress</TableHead>
+                  <TableHead>Date</TableHead>
+                  <TableHead className="text-right">comment</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell className="font-medium">INV001</TableCell>
+                  <TableCell>Paid</TableCell>
+                  <TableCell>
+                    <Progress value={56} />
+                  </TableCell>
+                  <TableCell>Credit Card</TableCell>
+                  <TableCell className="text-right">$250.00</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+            <div className="h-32 border border-slate-500 rounded-xl flex items-center">
+              <div className="ml-5 p-6 shadow-sm shadow-slate-400 rounded-lg">
+                <FaMoneyBillTrendUp className="w-12 h-12" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
