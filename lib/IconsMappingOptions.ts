2ebb3f67b2fea0bc8ff00c62ef6a0f5ea7e8629b
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-23 12:27:49 +0300

    Created IconMappingOptions.ts and added icons for income

---
+++ lib/IconsMappingOptions.ts
@@ -0,0 +1,46 @@
+import { MdWork } from "react-icons/md";
+import { FaMoneyBillTrendUp, FaMoneyCheckDollar } from "react-icons/fa6";
+import { GrMoney } from "react-icons/gr";
+import { MdWorkHistory } from "react-icons/md";
+import { BsCurrencyBitcoin, BsGraphUpArrow } from "react-icons/bs";
+import { RiBankLine } from "react-icons/ri";
+import { AiOutlineQuestionCircle } from "react-icons/ai";
+
+export const income = [
+  {
+    value: "salary",
+    icon: MdWork,
+  },
+  {
+    value: "investment",
+    icon: FaMoneyBillTrendUp,
+  },
+  {
+    value: "side-business",
+    icon: GrMoney,
+  },
+  {
+    value: "freelance",
+    icon: MdWorkHistory,
+  },
+  {
+    value: "stocks",
+    icon: BsGraphUpArrow,
+  },
+  {
+    value: "crypto",
+    icon: BsCurrencyBitcoin,
+  },
+  {
+    value: "bonds",
+    icon: RiBankLine,
+  },
+  {
+    value: "pension",
+    icon: FaMoneyCheckDollar,
+  },
+  {
+    value: "other",
+    icon: AiOutlineQuestionCircle,
+  },
+];
