a69c4fc1f804c42adb0c785833b69f09c3de94f0
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-23 11:29:11 +0300

    Imported AddExpenseModal to the expense page

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -1,10 +1,14 @@
+import AddExpenseModal from "@/components/AddExpenseModal";
 import Navbar2 from "@/components/Navbar2";
 
 export default function page() {
   return (
     <>
       <Navbar2 />
-      <div className="container mx-auto mt-4 px-12 pb-5 pt-12">Expenses</div>
+      <div className="container mx-auto mt-4 px-12 pb-5 pt-12">
+        <div>Expenses</div>
+        <AddExpenseModal />
+      </div>
     </>
   );
 }
