45c77f67c9a8eb50d28553fcd01d74300ab98ca6
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-23 11:25:50 +0300

    Designed AddExpense Form

--- components/AddExpenseModal.tsx
+++ components/AddExpenseModal.tsx
@@ -32,7 +32,7 @@ import { format } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
 
-export default function AddIncomeModal() {
+export default function AddExpenseModal() {
   const [date, setDate] = useState<Date>();
 
   return (
@@ -41,12 +41,12 @@ export default function AddIncomeModal() {
         <DialogTrigger asChild>
           <Button className="flex items-center space-x-2 rounded-3xl">
             <AiOutlinePlus className="w-4 h-4 text-white" />
-            <span>Add Income</span>
+            <span>Add Expense</span>
           </Button>
         </DialogTrigger>
         <DialogContent className="max-w-2xl">
           <DialogHeader>
-            <DialogTitle>Add Income</DialogTitle>
+            <DialogTitle>Add Expense</DialogTitle>
             {/* <DialogDescription>
               Make changes to your profile here. Click save when you are done.
             </DialogDescription> */}
@@ -54,21 +54,21 @@ export default function AddIncomeModal() {
           <form className="space-y-3 px-3">
             <div className="grid md:grid-cols-2 md:gap-6">
               <div className="relative">
-                <Label htmlFor="income-name" className="text-right">
-                  Income Name
+                <Label htmlFor="expense-name" className="text-right">
+                  Expense Name
                 </Label>
-                <Input id="income-name" className="" />
+                <Input id="expense-name" className="" />
               </div>
               <div className="relative">
-                <Label htmlFor="income-amount" className="text-right">
-                  Income Amount
+                <Label htmlFor="expense-amount" className="text-right">
+                  Expense Amount
                 </Label>
-                <Input id="income-amount" className="" />
+                <Input id="expense-amount" className="" />
               </div>
               <div className="grid grid-cols-1">
                 {/* Use flex to align label and popover content */}
-                <Label htmlFor="income-amount" className="text-left">
-                  Income Date
+                <Label htmlFor="expense-amount" className="text-left">
+                  Expense Date
                 </Label>
                 <Popover>
                   <PopoverTrigger asChild>
@@ -94,40 +94,45 @@ export default function AddIncomeModal() {
                 </Popover>
               </div>
               <div className="grid grid-cols-1">
-                <Label htmlFor="income-amount" className="text-left">
-                  Income Category
+                <Label htmlFor="expense-amount" className="text-left">
+                  Expense Category
                 </Label>
                 <Select>
                   <SelectTrigger>
-                    <SelectValue placeholder="Select income category" />
+                    <SelectValue placeholder="Select expense category" />
                   </SelectTrigger>
                   <SelectContent>
-                    <SelectItem value="salary">Salary</SelectItem>
+                    <SelectItem value="housing">Housing</SelectItem>
+                    <SelectItem value="transport">Transport</SelectItem>
+                    <SelectItem value="healthcare">Healthcare</SelectItem>
+                    <SelectItem value="education">Education</SelectItem>
+                    <SelectItem value="loan">Loan</SelectItem>
+                    <SelectItem value="food">Food & Groceries</SelectItem>
+                    <SelectItem value="insurance">Insurance</SelectItem>
+                    <SelectItem value="utilities">Utilities</SelectItem>
+                    <SelectItem value="clothing">Clothing</SelectItem>
+                    <SelectItem value="entertainment">Entertainment</SelectItem>
+                    <SelectItem value="savings">Savings</SelectItem>
                     <SelectItem value="investment">Investment</SelectItem>
-                    <SelectItem value="side-business">Side Business</SelectItem>
-                    <SelectItem value="freelance">Freelance</SelectItem>
-                    <SelectItem value="stocks">Stocks</SelectItem>
-                    <SelectItem value="crypto">Cryptocurrency</SelectItem>
-                    <SelectItem value="bonds">Bonds</SelectItem>
-                    <SelectItem value="pension">Pension</SelectItem>
+                    <SelectItem value="vacation">Vacation</SelectItem>
                     <SelectItem value="other">Other</SelectItem>
                   </SelectContent>
                 </Select>
               </div>
             </div>
             <div>
-              <Label htmlFor="income-description" className="text-right">
+              <Label htmlFor="expense-amount" className="text-right">
                 Description
               </Label>
-              <Textarea id="income-description" />
+              <Textarea id="expense-description" />
             </div>
             {/* <div className="flex">
               <Button className="ml-auto " type="submit">
-                Add Income
+                Add Expense
               </Button>
             </div> */}
             <DialogFooter>
-              <Button type="submit">Save Income</Button>
+              <Button type="submit">Save Expense</Button>
             </DialogFooter>
           </form>
         </DialogContent>
