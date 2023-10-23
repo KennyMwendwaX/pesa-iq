ee8d11431f433e59d5a2958a24fea52fb84380ca
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-23 11:58:37 +0300

    npm i zod @hookform/resolvers

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -1,4 +1,5 @@
 import AddExpenseModal from "@/components/AddExpenseModal";
+import ExpenseCard from "@/components/ExpenseCard";
 import Navbar2 from "@/components/Navbar2";
 
 export default function page() {
@@ -8,6 +9,11 @@ export default function page() {
       <div className="container mx-auto mt-4 px-12 pb-5 pt-12">
         <div>Expenses</div>
         <AddExpenseModal />
+        <div className="flex pt-3">
+          <div className="w-[800px]">
+            <ExpenseCard />
+          </div>
+        </div>
       </div>
     </>
   );
