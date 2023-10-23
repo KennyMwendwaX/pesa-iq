c3d627eae96dffc8a97a170ed6b7a7892dc9436b
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-23 13:05:26 +0300

    Added expenseCategories to IconMappingOptions.ts

--- lib/IconsMappingOptions.ts
+++ lib/IconsMappingOptions.ts
@@ -1,12 +1,30 @@
-import { MdWork } from "react-icons/md";
-import { FaMoneyBillTrendUp, FaMoneyCheckDollar } from "react-icons/fa6";
+import { MdHealthAndSafety, MdWork } from "react-icons/md";
+import {
+  FaMoneyBillTrendUp,
+  FaMoneyCheckDollar,
+  FaUmbrellaBeach,
+} from "react-icons/fa6";
 import { GrMoney } from "react-icons/gr";
 import { MdWorkHistory } from "react-icons/md";
-import { BsCurrencyBitcoin, BsGraphUpArrow } from "react-icons/bs";
+import {
+  BsCarFrontFill,
+  BsCurrencyBitcoin,
+  BsFillTagFill,
+  BsGraphUpArrow,
+  BsShieldCheck,
+} from "react-icons/bs";
 import { RiBankLine } from "react-icons/ri";
-import { AiOutlineQuestionCircle } from "react-icons/ai";
+import {
+  AiOutlineBulb,
+  AiOutlineDollarCircle,
+  AiOutlineQuestionCircle,
+} from "react-icons/ai";
+import { IoHome } from "react-icons/io5";
+import { BiSolidBookBookmark, BiSolidMoviePlay } from "react-icons/bi";
+import { GiKnifeFork } from "react-icons/gi";
+import { CiCoinInsert } from "react-icons/ci";
 
-export const income = [
+export const incomeCategories = [
   {
     value: "salary",
     icon: MdWork,
@@ -38,6 +56,65 @@ export const income = [
   {
     value: "pension",
     icon: FaMoneyCheckDollar,
+  },
+  {
+    value: "other",
+    icon: AiOutlineQuestionCircle,
+  },
+];
+
+export const expenseCategories = [
+  {
+    value: "housing",
+    icon: IoHome,
+  },
+  {
+    value: "transport",
+    icon: BsCarFrontFill,
+  },
+  {
+    value: "healthcare",
+    icon: MdHealthAndSafety,
+  },
+  {
+    value: "education",
+    icon: BiSolidBookBookmark,
+  },
+  {
+    value: "loan",
+    icon: AiOutlineDollarCircle,
+  },
+  {
+    value: "food",
+    icon: GiKnifeFork,
+  },
+  {
+    value: "insurance",
+    icon: BsShieldCheck,
+  },
+  {
+    value: "utilities",
+    icon: AiOutlineBulb,
+  },
+  {
+    value: "clothing",
+    icon: BsFillTagFill,
+  },
+  {
+    value: "entertainment",
+    icon: BiSolidMoviePlay,
+  },
+  {
+    value: "savings",
+    icon: CiCoinInsert,
+  },
+  {
+    value: "investment",
+    icon: FaMoneyBillTrendUp,
+  },
+  {
+    value: "vacation",
+    icon: FaUmbrellaBeach,
   },
   {
     value: "other",
