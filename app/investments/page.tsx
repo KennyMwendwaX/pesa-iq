b978d5a16c6998c3da0d18a23ed0dbc204b1bece
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-25 19:06:13 +0300

    Added navbar and container to investments page

--- app/investments/page.tsx
+++ app/investments/page.tsx
@@ -1,7 +1,10 @@
+import Navbar2 from "@/components/Navbar2";
+
 export default function Investment() {
   return (
     <>
-      <div>page</div>
+      <Navbar2 />
+      <div className="container mx-auto mt-4 px-12 pb-5 pt-12">page</div>
     </>
   );
 }
