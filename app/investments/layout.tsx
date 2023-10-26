627e35128c337861bf8a56d7fb82f94850eb0d59
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-26 18:46:18 +0300

    CreaAdded metadata to investments/layout.tsx

--- app/investments/layout.tsx
+++ app/investments/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Investment Hub",
+  description: "Investment hub page",
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
