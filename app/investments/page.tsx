f8a8151af15ab4f020b2089653c829c5594360da
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-27 21:33:17 +0300

    Added investment-hub title and description

--- app/investments/page.tsx
+++ app/investments/page.tsx
@@ -5,14 +5,15 @@ export default function Investment() {
     <>
       <Navbar />
       <div className="container mx-auto mt-4 px-12 pb-5 pt-12">
-        <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+        <div className="py-4 px-4 mx-auto max-w-screen-xl lg:px-6">
           <div className="max-w-screen-md mb-8 lg:mb-16">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-              Designed for business teams like yours
+              Discover Your Investment Hub
             </h2>
             <p className="text-gray-500 sm:text-xl dark:text-gray-400">
-              Here at Flowbite we focus on markets where technology, innovation,
-              and capital can unlock long-term value and drive economic growth.
+              Explore a world of investment opportunities, your gateway to
+              financial success. Empower your financial journey with our
+              valuable resources and insights.
             </p>
           </div>
           <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
