7a25234f46bbf31ce0d696a6d3249e053187bd24
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-27 21:20:16 +0300

    Added Investment categories forex to the investment-hub

--- app/investments/page.tsx
+++ app/investments/page.tsx
@@ -132,6 +132,26 @@ export default function Investment() {
                 individual.
               </p>
             </div>
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                <svg
+                  className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fill-rule="evenodd"
+                    d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+                    clip-rule="evenodd"></path>
+                </svg>
+              </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Forex</h3>
+              <p className="text-gray-500 dark:text-gray-400">
+                Keep your company’s lights on with customizable, iterative, and
+                structured workflows built for all efficient teams and
+                individual.
+              </p>
+            </div>
           </div>
         </div>
       </div>
