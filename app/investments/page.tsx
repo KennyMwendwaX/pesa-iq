b80ef8aac0fa009076a2b1a36bb2642405058281
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-28 16:57:28 +0300

    Added the icon for NSE, saccos and forex at the investment categories

--- app/investments/page.tsx
+++ app/investments/page.tsx
@@ -1,6 +1,9 @@
 import Navbar from "@/components/Navbar";
+import { AiFillMoneyCollect } from "react-icons/ai";
+import { HiCurrencyDollar, HiPresentationChartLine } from "react-icons/hi";
 import {
   FaBitcoin,
+  FaHandshake,
   FaMoneyBillTrendUp,
   FaMoneyCheckDollar,
 } from "react-icons/fa6";
@@ -24,7 +27,7 @@ export default function Investment() {
           <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
             <div>
               <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
-                <svg
+                {/* <svg
                   className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300"
                   fill="currentColor"
                   viewBox="0 0 20 20"
@@ -33,7 +36,8 @@ export default function Investment() {
                     fill-rule="evenodd"
                     d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
                     clip-rule="evenodd"></path>
-                </svg>
+                </svg> */}
+                <HiPresentationChartLine className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" />
               </div>
               <h3 className="mb-2 text-xl font-bold dark:text-white">NSE</h3>
               <p className="text-gray-500 dark:text-gray-400">
@@ -54,18 +58,7 @@ export default function Investment() {
             </div>
             <div>
               <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
-                <svg
-                  className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    fill-rule="evenodd"
-                    d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
-                    clip-rule="evenodd"></path>
-                  <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path>
-                </svg>
-                <FaBitcoin className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" />
+                <HiCurrencyDollar className="w-6 h-6 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" />
               </div>
               <h3 className="mb-2 text-xl font-bold dark:text-white">
                 Stock Market
@@ -77,7 +70,7 @@ export default function Investment() {
             </div>
             <div>
               <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
-                <svg
+                {/* <svg
                   className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300"
                   fill="currentColor"
                   viewBox="0 0 20 20"
@@ -87,7 +80,8 @@ export default function Investment() {
                     fill-rule="evenodd"
                     d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z"
                     clip-rule="evenodd"></path>
-                </svg>
+                </svg> */}
+                <FaHandshake className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" />
               </div>
               <h3 className="mb-2 text-xl font-bold dark:text-white">Saccos</h3>
               <p className="text-gray-500 dark:text-gray-400">
@@ -137,7 +131,7 @@ export default function Investment() {
             </div>
             <div>
               <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
-                <svg
+                {/* <svg
                   className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300"
                   fill="currentColor"
                   viewBox="0 0 20 20"
@@ -146,7 +140,8 @@ export default function Investment() {
                     fill-rule="evenodd"
                     d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
                     clip-rule="evenodd"></path>
-                </svg>
+                </svg> */}
+                <AiFillMoneyCollect className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" />
               </div>
               <h3 className="mb-2 text-xl font-bold dark:text-white">Forex</h3>
               <p className="text-gray-500 dark:text-gray-400">
