43f8ab31fcabd6629eb8541e49b95ec53e56fae5
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-28 16:32:53 +0300

    Added the bitcoin icon at the investment crypto category

--- app/investments/page.tsx
+++ app/investments/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import { FaBitcoin } from "react-icons/fa6";
 
 export default function Investment() {
   return (
@@ -39,13 +40,7 @@ export default function Investment() {
             </div>
             <div>
               <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
-                <svg
-                  className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path>
-                </svg>
+                <FaBitcoin className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" />
               </div>
               <h3 className="mb-2 text-xl font-bold dark:text-white">Crypto</h3>
               <p className="text-gray-500 dark:text-gray-400">
@@ -66,6 +61,7 @@ export default function Investment() {
                     clip-rule="evenodd"></path>
                   <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path>
                 </svg>
+                <FaBitcoin className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" />
               </div>
               <h3 className="mb-2 text-xl font-bold dark:text-white">
                 Stock Market
