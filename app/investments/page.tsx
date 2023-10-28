ac9e234e34beb0e9f528e3a13ac004602e8ac072
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-28 16:39:51 +0300

    Added the icon for money market fund and for bonds at the investment crypto category

--- app/investments/page.tsx
+++ app/investments/page.tsx
@@ -1,5 +1,9 @@
 import Navbar from "@/components/Navbar";
-import { FaBitcoin } from "react-icons/fa6";
+import {
+  FaBitcoin,
+  FaMoneyBillTrendUp,
+  FaMoneyCheckDollar,
+} from "react-icons/fa6";
 
 export default function Investment() {
   return (
@@ -93,13 +97,14 @@ export default function Investment() {
             </div>
             <div>
               <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
-                <svg
+                {/* <svg
                   className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg">
                   <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path>
-                </svg>
+                </svg> */}
+                <FaMoneyBillTrendUp className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" />
               </div>
               <h3 className="mb-2 text-xl font-bold dark:text-white">
                 Money Market Fund
@@ -111,7 +116,7 @@ export default function Investment() {
             </div>
             <div>
               <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
-                <svg
+                {/* <svg
                   className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300"
                   fill="currentColor"
                   viewBox="0 0 20 20"
@@ -120,7 +125,8 @@ export default function Investment() {
                     fill-rule="evenodd"
                     d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
                     clip-rule="evenodd"></path>
-                </svg>
+                </svg> */}
+                <FaMoneyCheckDollar className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" />
               </div>
               <h3 className="mb-2 text-xl font-bold dark:text-white">Bonds</h3>
               <p className="text-gray-500 dark:text-gray-400">
