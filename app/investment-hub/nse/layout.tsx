7713d3e0188b37e21e64de56148839c735c1a181
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-29 17:35:26 +0300

    Added a metadata to layout.tsx to nse dir

--- app/investment-hub/nse/layout.tsx
+++ app/investment-hub/nse/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Nairobi Stock Exchange",
+  description: "Nairobi Stock Exchange investment hub page",
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
