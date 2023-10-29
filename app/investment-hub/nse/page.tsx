c6ab36b62034c25a31dccf10c8ca17ad7fe57626
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-29 17:30:13 +0300

    Created NSE investment page

---
+++ app/investment-hub/nse/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar";
+
+export default function NSE() {
+  return (
+    <>
+      <Navbar />
+      <div className="container mx-auto mt-4 px-12 pb-5 pt-12">NSE</div>
+    </>
+  );
+}
