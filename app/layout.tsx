264f8d074d143900503a248b94847917d66e2039
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-30 11:19:32 +0300

    Added the Navbar.tsx to the root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={roboto.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
