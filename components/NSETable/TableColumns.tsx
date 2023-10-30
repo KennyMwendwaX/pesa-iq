edb2f1f26c8849ce133bc6122ad2bda88e92a268
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-30 17:09:15 +0300

    Added TableColumns.tsx component

---
+++ components/NSETable/TableColumns.tsx
@@ -0,0 +1,54 @@
+import { ColumnDef } from "@tanstack/react-table";
+
+import { Checkbox } from "@/components/ui/checkbox";
+import TableColumnHeader from "./TableColumnHeader";
+import type { NSEStockData } from "@/types/NSEStockData";
+
+export const TableColumns: ColumnDef<NSEStockData>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select-all"
+        className="translate-y-[2px]"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select-row"
+        className="translate-y-[2px]"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "ticker",
+    header: () => <TableColumnHeader name="Ticker" />,
+    cell: ({ row }) => <div className="w-[50px]">{row.getValue("ticker")}</div>,
+  },
+  {
+    accessorKey: "name",
+    header: () => <TableColumnHeader name="Name" />,
+    cell: ({ row }) => <div className="">{row.getValue("name")}</div>,
+  },
+  {
+    accessorKey: "volume",
+    header: () => <TableColumnHeader name="Volume" />,
+    cell: ({ row }) => <div className="">{row.getValue("volume")}</div>,
+  },
+  {
+    accessorKey: "price",
+    header: () => <TableColumnHeader name="Price" />,
+    cell: ({ row }) => <div className="">{row.getValue("price")}</div>,
+  },
+  {
+    accessorKey: "change",
+    header: () => <TableColumnHeader name="Change" />,
+    cell: ({ row }) => <div className="">{row.getValue("change")}</div>,
+  },
+];
