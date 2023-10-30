c70b80d69a4ad95a4fb79673e1296974b79437e9
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-30 11:39:45 +0300

    Added a new QueryClient

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,15 @@ import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const roboto = Roboto({
   weight: ["400"],
   subsets: ["latin"],
 });
+
+// Create a client
+const queryClient = new QueryClient();
 
 export const metadata: Metadata = {
   title: "PesaIQ",
@@ -21,8 +25,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <Navbar />
-        {children}
+        <QueryClientProvider client={queryClient}>
+          <Navbar />
+          {children}
+        </QueryClientProvider>
       </body>
     </html>
   );
