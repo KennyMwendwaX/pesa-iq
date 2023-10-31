f44df437511ec087eff19f1a79109a0c1a2c3037
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-31 10:12:53 +0300

    Added TableFilter.tsx

---
+++ components/NSETable/TableFilter.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Table } from "@tanstack/react-table";
+import { Input } from "@/components/ui/input";
+
+interface TableToolbarProps<TData> {
+  table: Table<TData>;
+}
+
+export default function TableToolbar<TData>({
+  table,
+}: TableToolbarProps<TData>) {
+  const isFiltered = table.getState().columnFilters.length > 0;
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <div className="flex flex-1 items-center space-x-2">
+          <Input
+            placeholder="Search tasks..."
+            value={(table.getColumn("title")?.getFilterValue() as string) ?? ""}
+            onChange={(event) =>
+              table.getColumn("title")?.setFilterValue(event.target.value)
+            }
+            className="h-8 w-[150px] lg:w-[250px]"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
