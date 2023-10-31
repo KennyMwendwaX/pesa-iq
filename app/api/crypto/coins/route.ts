ea75fa794b1cd54bc48ef2f42b664a1c65be0e95
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-31 18:30:20 +0300

    Created api/crypto/coins to fetch crypto coins data

--- app/api/crypto/coins/route.ts
+++ app/api/crypto/coins/route.ts
@@ -1,3 +1,39 @@
 import { NextResponse } from "next/server";
+import axios from "axios";
 
-export async function GET() {}
+export async function GET() {
+  const options = {
+    method: "GET",
+    url: "https://coinranking1.p.rapidapi.com/coins",
+    params: {
+      referenceCurrencyUuid: "yhjMzLPhuIDl",
+      timePeriod: "24h",
+      "tiers[0]": "1",
+      orderBy: "marketCap",
+      orderDirection: "desc",
+      limit: "50",
+      offset: "0",
+    },
+    headers: {
+      "X-RapidAPI-Key": "7bfd5fe57bmsh7905ed23d600c05p12dc8ajsne2b2ea67ac13",
+      "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    if (!response)
+      return NextResponse.json(
+        { message: "Failed to fetch crypto coins" },
+        { status: 400 }
+      );
+    const coins = response.data;
+    return NextResponse.json({ coins }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Server error, try again later" },
+      { status: 500 }
+    );
+  }
+}
