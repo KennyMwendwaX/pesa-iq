b824109c00b47aa52f3e366ede7af4fad1659d63
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-31 18:16:34 +0300

    Replaced fetch with axios in api/nse route

--- app/api/nse/route.ts
+++ app/api/nse/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
+import axios from "axios";
 
 export async function GET() {
-  const url = "https://nairobi-stock-exchange-nse.p.rapidapi.com/stocks";
   const options = {
     method: "GET",
+    url: "https://nairobi-stock-exchange-nse.p.rapidapi.com/stocks",
     headers: {
       "X-RapidAPI-Key": "7bfd5fe57bmsh7905ed23d600c05p12dc8ajsne2b2ea67ac13",
       "X-RapidAPI-Host": "nairobi-stock-exchange-nse.p.rapidapi.com",
@@ -11,13 +12,13 @@ export async function GET() {
   };
 
   try {
-    const response = await fetch(url, options);
+    const response = await axios.request(options);
     if (!response)
       return NextResponse.json(
         { message: "Failed to fetch NSE stocks" },
         { status: 400 }
       );
-    const NSE_Stocks = await response.json();
+    const NSE_Stocks = response.data;
     return NextResponse.json({ NSE_Stocks }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
