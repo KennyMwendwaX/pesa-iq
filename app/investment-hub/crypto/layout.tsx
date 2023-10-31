f824fdd97055d31f8f0f2e963bcaf9881e908586
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-31 17:21:33 +0300

    Added metadata to layout.tsx of the crypto page

--- app/investment-hub/crypto/layout.tsx
+++ app/investment-hub/crypto/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Cryptocurrency",
+  description: "Cryptocurrency investment hub page",
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
