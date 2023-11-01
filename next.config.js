be2bbc6bd04976bed6fa0eb0b1a250d0332abcdd
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-01 11:12:49 +0300

    Added the coinranking image domain to next config for crypto svgs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "lh3.googleusercontent.com",
-      "avatars.githubusercontent.com",
-      "cdn.coinranking.com",
+    // domains: [
+    //   "lh3.googleusercontent.com",
+    //   "avatars.githubusercontent.com",
+    //   "cdn.coinranking.com",
+    // ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.coinranking.com",
+      },
     ],
   },
 };
