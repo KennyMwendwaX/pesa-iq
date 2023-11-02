1a024fdbdfbae52f76d3861a700ef17488cf3f1e
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-02 13:35:03 +0300

    Added TableColumns.tsx

---
+++ components/StockTable/TableColumns.tsx
@@ -0,0 +1,66 @@
+import { ColumnDef } from "@tanstack/react-table";
+import Image from "next/image";
+import { Checkbox } from "@/components/ui/checkbox";
+import TableColumnHeader from "./TableColumnHeader";
+import type { Coin } from "@/types/Crypto";
+import { FiArrowDownRight, FiArrowRight, FiArrowUpRight } from "react-icons/fi";
+import formatCurrency from "@/lib/formatCurrency";
+
+export const CryptoTableColumns: ColumnDef<Coin>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select-all"
+        className="translate-y-[2px]"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select-row"
+        className="translate-y-[2px]"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "symbol",
+    header: () => <TableColumnHeader name="Symbol" />,
+    cell: ({ row }) => <div className="w-[30px]">{row.getValue("rank")}</div>,
+  },
+  {
+    accessorKey: "name",
+    header: () => <TableColumnHeader name="Name" />,
+    cell: ({ row }) => <div className="">{row.getValue("name")}</div>,
+  },
+  {
+    accessorKey: "currency",
+    header: () => <TableColumnHeader name="Currency" />,
+    cell: ({ row }) => <div className="">{row.getValue("currency")}</div>,
+  },
+  {
+    accessorKey: "exchange",
+    header: () => <TableColumnHeader name="Exchange" />,
+    cell: ({ row }) => <div className="">{row.getValue("exchange")}</div>,
+  },
+  {
+    accessorKey: "mic_code",
+    header: () => <TableColumnHeader name="Mic Code" />,
+    cell: ({ row }) => <div className="">{row.getValue("mic_code")}</div>,
+  },
+  {
+    accessorKey: "country",
+    header: () => <TableColumnHeader name="Country" />,
+    cell: ({ row }) => <div className="">{row.getValue("country")}</div>,
+  },
+  {
+    accessorKey: "type",
+    header: () => <TableColumnHeader name="Type" />,
+    cell: ({ row }) => <div className="">{row.getValue("type")}</div>,
+  },
+];
