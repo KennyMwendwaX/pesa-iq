248259e498f9239732395a7aaa1ff8360fe2ff74
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-02 12:54:07 +0300

    Added my api key for the route

--- app/api/stocks/route.ts
+++ app/api/stocks/route.ts
@@ -4,10 +4,10 @@ import axios from "axios";
 export async function GET() {
   const options = {
     method: "GET",
-    url: "https://nairobi-stock-exchange-nse.p.rapidapi.com/stocks/Safaricom",
+    url: "https://twelve-data1.p.rapidapi.com/market_movers/stocks",
     headers: {
       "X-RapidAPI-Key": "7bfd5fe57bmsh7905ed23d600c05p12dc8ajsne2b2ea67ac13",
-      "X-RapidAPI-Host": "nairobi-stock-exchange-nse.p.rapidapi.com",
+      "X-RapidAPI-Host": "twelve-data1.p.rapidapi.com",
     },
   };
 
@@ -18,8 +18,8 @@ export async function GET() {
         { message: "Failed to fetch NSE stocks" },
         { status: 400 }
       );
-    const NSE_Stocks = response.data;
-    return NextResponse.json({ NSE_Stocks }, { status: 200 });
+    const stocks = response.data;
+    return NextResponse.json({ stocks }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { message: "Server error, try again later" },
