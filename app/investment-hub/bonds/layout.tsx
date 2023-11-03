4054cc661f14049ab132839209dee70114af5cf1
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-03 13:13:31 +0300

    Added metadata to the layout of the bonds page

--- app/investment-hub/bonds/layout.tsx
+++ app/investment-hub/bonds/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Bonds",
+  description: "Analysing the treasury bonds",
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
