a6d95b2913d53dbcfd479433cf8be94b9cca42ac
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-03 12:15:16 +0300

    Added metadata to the layout of the money-market-fund page

--- app/investment-hub/money-market-fund/layout.tsx
+++ app/investment-hub/money-market-fund/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Money Market Fund",
+  description: "Top 15 best performing Money Market Fund in 2023",
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
