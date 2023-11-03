241e62307688e60251404bdb014b67c2d58f1180
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-03 07:18:04 +0300

    Displayed the sacco table on the sacco page

--- app/investment-hub/saccos/page.tsx
+++ app/investment-hub/saccos/page.tsx
@@ -1,27 +1,30 @@
 "use client";
 
-import CryptoOverview from "@/components/CryptoOverview";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import type { CoinData } from "@/types/Crypto";
-import CryptoTable from "@/components/CryptoTable/Table";
-import { CryptoTableColumns } from "@/components/CryptoTable/TableColumns";
+import SaccoTable from "@/components/SaccoTable/Table";
+import { SaccoTableColumns } from "@/components/SaccoTable/TableColumns";
+import { saccos } from "@/lib/saccos";
 
 export default function Saccos() {
-  //   const { data } = useQuery({
-  //     queryKey: ["coins"],
-  //     queryFn: async () => {
-  //       const { data } = await axios.get("/api/crypto/coins");
-  //       return data.coins as CoinData;
-  //     },
-  //   });
-
-  //   const stats = data?.data.stats;
-  //   const coinData = data?.data.coins;
-
   return (
     <>
-      <div className="container mx-auto mt-4 px-12 pb-5 pt-12">Saccos</div>
+      <div className="container mx-auto mt-4 px-12 pb-5 pt-12">
+        <div className="flex items-center justify-between space-y-2 pb-3">
+          <div>
+            <div className="text-2xl font-bold tracking-tight">
+              Top 20 best performing Saccos in 2023
+            </div>
+            <p className="text-muted-foreground">
+              Here&apos;s a list of best Saccos ranked by dividends on share
+              capital!
+            </p>
+          </div>
+        </div>
+        {saccos && saccos.length > 0 ? (
+          <SaccoTable data={saccos} columns={SaccoTableColumns} />
+        ) : (
+          <p>No data available.</p>
+        )}
+      </div>
     </>
   );
 }
