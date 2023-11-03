546bc853349c910ecf2a0827f714c3f90cc3766a
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-03 11:35:07 +0300

    Added metadata to the layout of the sacco page

--- app/investment-hub/saccos/layout.tsx
+++ app/investment-hub/saccos/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "SACCOS",
+  description: "Top 20 best performing saccos in 2023",
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
