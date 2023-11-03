57534072d7ac2463c43f9736af51b30cc202514b
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-03 06:55:21 +0300

    Added type Sacco

---
+++ components/SaccoTable/TableColumns.tsx
@@ -0,0 +1,89 @@
+import { ColumnDef } from "@tanstack/react-table";
+
+import { Checkbox } from "@/components/ui/checkbox";
+import TableColumnHeader from "./TableColumnHeader";
+import type { NSEStockData } from "@/types/NSEStockData";
+import { FiArrowDownRight, FiArrowRight, FiArrowUpRight } from "react-icons/fi";
+
+export const SaccoTableColumns: ColumnDef<NSEStockData>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select-all"
+        className="translate-y-[2px]"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select-row"
+        className="translate-y-[2px]"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "ticker",
+    header: () => <TableColumnHeader name="Ticker" />,
+    cell: ({ row }) => <div className="w-[80px]">{row.getValue("ticker")}</div>,
+  },
+  {
+    accessorKey: "name",
+    header: () => <TableColumnHeader name="Name" />,
+    cell: ({ row }) => <div className="">{row.getValue("name")}</div>,
+  },
+  {
+    accessorKey: "volume",
+    header: () => <TableColumnHeader name="Volume" />,
+    cell: ({ row }) => {
+      const volume = parseInt(row.getValue("volume"));
+
+      return (
+        <>{volume > 0 ? <div>{row.getValue("volume")}</div> : <div>0</div>}</>
+      );
+    },
+  },
+  {
+    accessorKey: "price",
+    header: () => <TableColumnHeader name="Price" />,
+    cell: ({ row }) => <div className="">{row.getValue("price")}</div>,
+  },
+  {
+    accessorKey: "change",
+    header: () => <TableColumnHeader name="Change" />,
+    cell: ({ row }) => {
+      const changeValue = parseFloat(row.getValue("change"));
+
+      return (
+        <>
+          {changeValue < 0.0 ? (
+            <div className="flex items-center">
+              <FiArrowDownRight className="w-5 h-5 mr-2 text-red-600" />
+              <div className="text-red-600">{row.getValue("change")}</div>
+            </div>
+          ) : changeValue > 0.0 ? (
+            <div className="flex items-center">
+              <FiArrowUpRight className="w-5 h-5 mr-2 text-green-600" />
+              <div className="text-green-600">{row.getValue("change")}</div>
+            </div>
+          ) : changeValue == 0.0 ? (
+            <div className="flex items-center">
+              <FiArrowRight className="w-5 h-5 mr-2 text-gray-600" />
+              <div className="text-gray-600">0.00</div>
+            </div>
+          ) : (
+            <div className="flex items-center">
+              <FiArrowRight className="w-5 h-5 mr-2 text-gray-600" />
+              <div className="text-gray-600">0.00</div>
+            </div>
+          )}
+        </>
+      );
+    },
+  },
+];
---
+++ types/Sacco.ts
@@ -0,0 +1,10 @@
+export type Sacco = {
+  rank: number;
+  sacco: string;
+  registration_fee: number;
+  minimum_monthly_contribution: number;
+  share_on_capital: number;
+  maximum_loan_multiple: number;
+  dividend_on_share_capital: string;
+  interest_in_member_deposit: string;
+};
