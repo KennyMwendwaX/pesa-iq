c5de5d8a3e1c8522780d06968b98b157f2ae6d49
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-03 11:48:44 +0300

    Added MoneyMarketFund.ts for MoneyMarketFund types

---
+++ lib/MoneyMarketFund.ts
@@ -0,0 +1,8 @@
+export const moneyMarketFund = [
+  {
+    rank: 1,
+    name: "Enwealth Money Market Fund",
+    fund_manager: "Enwealth Financial Services",
+    average_daily_yield: "11.43%",
+  },
+];
---
+++ types/MoneyMarketFund.ts
@@ -0,0 +1,6 @@
+export type MoneyMarketFund = {
+  rank: number;
+  name: string;
+  fund_manager: "Enwealth Financial Services";
+  average_daily_yield: "11.43%";
+};
