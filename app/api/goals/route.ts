e45bcf0cd06ce4b2e186d0be87cc5b0a99ca6b49
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-07 13:15:59 +0300

    Added goals api GET route

---
+++ app/api/goals/route.ts
@@ -0,0 +1,67 @@
+import prisma from "@/prisma/db";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const goalList = await prisma.goal.findMany();
+    // Return success message
+    if (goalList) {
+      return NextResponse.json({ goalList }, { status: 200 });
+    } else {
+      return NextResponse.json(
+        { message: "Failed to return expense list" },
+        { status: 404 }
+      );
+    }
+  } catch (error) {}
+  return NextResponse.json(
+    { message: "Server error, try again later" },
+    { status: 500 }
+  );
+}
+
+export async function POST(request: Request) {
+  const req = await request.json();
+  const {
+    name,
+    amount,
+    date,
+    category,
+    frequency,
+    transaction_type,
+    description,
+  } = req;
+
+  try {
+    // Create expense
+    const expense = await prisma.expense.create({
+      data: {
+        name: name,
+        amount: parseInt(amount as string),
+        category: category,
+        date: date,
+        frequency: frequency,
+        transaction_type: transaction_type,
+        description: description,
+      },
+    });
+
+    // Return success message
+    if (expense) {
+      return NextResponse.json(
+        { message: "Income registered successfully" },
+        { status: 201 }
+      );
+    } else {
+      return NextResponse.json(
+        { message: "Failed to register expense" },
+        { status: 500 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Server error, try again later" },
+      { status: 500 }
+    );
+  }
+}
