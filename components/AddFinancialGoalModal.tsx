70d929f164fa88eac59a7e88913042dc8641fc2f
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-07 09:11:52 +0300

    Added AddFinancialGoalModal.tsx

---
+++ components/AddFinancialGoalModal.tsx
@@ -0,0 +1,293 @@
+"use client";
+
+import { useState } from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Textarea } from "@/components/ui/textarea";
+import { AiOutlinePlus } from "react-icons/ai";
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useForm } from "react-hook-form";
+
+export default function AddFinancialGoalModal() {
+  const form = useForm();
+  const [isDialogOpen, setDialogOpen] = useState(false);
+
+  // toggle the dialog open state
+  const toggleDialog = () => {
+    setDialogOpen(!isDialogOpen);
+  };
+
+  async function onSubmit() {
+    toggleDialog();
+  }
+
+  return (
+    <>
+      <Dialog open={isDialogOpen} onOpenChange={toggleDialog}>
+        <DialogTrigger asChild>
+          <Button className="flex items-center space-x-2 rounded-3xl">
+            <AiOutlinePlus className="w-4 h-4 text-white" />
+            <span>Add Income</span>
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="max-w-2xl">
+          <DialogHeader>
+            <DialogTitle>Add Income</DialogTitle>
+            {/* <DialogDescription>
+              Make changes to your profile here. Click save when you are done.
+            </DialogDescription> */}
+          </DialogHeader>
+          <Form {...form}>
+            <form
+              className="space-y-3 px-3"
+              onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="grid md:grid-cols-2 md:gap-6">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Income Name</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="text"
+                          id="name"
+                          className="focus:border-2 focus:border-blue-600"
+                          placeholder="Income name"
+                          {...field}
+                          required
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="amount"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Income Amount</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="text"
+                          id="amount"
+                          className="focus:border-2 focus:border-blue-600"
+                          placeholder="Income amount"
+                          {...field}
+                          required
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="relative">
+                  {/* Use flex to align label and popover content */}
+                  <FormField
+                    control={form.control}
+                    name="date"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <FormLabel>Income Date</FormLabel>
+                        <Popover>
+                          <PopoverTrigger asChild>
+                            <FormControl>
+                              <Button
+                                variant={"outline"}
+                                className={cn(
+                                  "justify-start text-left font-normal",
+                                  !field.value && "text-muted-foreground"
+                                )}>
+                                {field.value ? (
+                                  format(field.value, "PPP")
+                                ) : (
+                                  <span>Pick a date</span>
+                                )}
+                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                              </Button>
+                            </FormControl>
+                          </PopoverTrigger>
+                          <PopoverContent className="w-auto p-0" align="start">
+                            <Calendar
+                              mode="single"
+                              selected={field.value}
+                              onSelect={field.onChange}
+                              initialFocus
+                            />
+                          </PopoverContent>
+                        </Popover>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="relative">
+                  <FormField
+                    control={form.control}
+                    name="category"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Income Category</FormLabel>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                          required>
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Select income category" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectItem value="salary">Salary</SelectItem>
+                            <SelectItem value="investment">
+                              Investment
+                            </SelectItem>
+                            <SelectItem value="side-business">
+                              Side Business
+                            </SelectItem>
+                            <SelectItem value="freelance">Freelance</SelectItem>
+                            <SelectItem value="stocks">Stocks</SelectItem>
+                            <SelectItem value="crypto">
+                              Cryptocurrency
+                            </SelectItem>
+                            <SelectItem value="bonds">Bonds</SelectItem>
+                            <SelectItem value="pension">Pension</SelectItem>
+                            <SelectItem value="other">Other</SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="relative">
+                  <FormField
+                    control={form.control}
+                    name="frequency"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Frequency on income</FormLabel>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                          required>
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Select the frequency of income" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectItem value="one-time">One Time</SelectItem>
+                            <SelectItem value="daily">Daily</SelectItem>
+                            <SelectItem value="weekly">Weekly</SelectItem>
+                            <SelectItem value="bi-weekly">Bi-Weekly</SelectItem>
+                            <SelectItem value="monthly">Monthly</SelectItem>
+                            <SelectItem value="yearly">Yearly</SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="relative">
+                  <FormField
+                    control={form.control}
+                    name="transaction_type"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Frequency on income</FormLabel>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                          required>
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Select the transaction type" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectItem value="cash">Cash</SelectItem>
+                            <SelectItem value="bank-transfer">
+                              Bank Transfer
+                            </SelectItem>
+                            <SelectItem value="mobile-banking">
+                              Mobile Banking
+                            </SelectItem>
+                            <SelectItem value="online-payments">
+                              Online Payment
+                            </SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </div>
+              <div>
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          id="description"
+                          className="focus:border-2 focus:border-blue-600"
+                          placeholder="Income description"
+                          {...field}
+                          required
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <DialogFooter>
+                <Button type="submit">Save Income</Button>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
