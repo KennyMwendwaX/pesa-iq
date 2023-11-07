36683941d3b85583ea68cb3a3d63f10fbaa94600
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-07 09:05:12 +0300

    Added metadata to layout for financial-goals

--- app/financial-goals/layout.tsx
+++ app/financial-goals/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Financial Goals",
+  description: "Track your financial goals",
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
