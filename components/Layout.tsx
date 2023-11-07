fa87a8eec0f102ca2e64a23346f984dd4dfda4bc
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-07 07:35:00 +0300

    Added layout.tsx

---
+++ components/Layout.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { usePathname } from "next/navigation";
+import Navbar from "./Navbar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const excludePaths = ["/", "/signup", "/signin"];
+
+  const excludedPaths = excludePaths.includes(pathname);
+
+  return (
+    <>
+      {excludedPaths ? (
+        <>{children}</>
+      ) : (
+        <>
+          <Navbar />
+          {children}
+        </>
+      )}
+    </>
+  );
+}
