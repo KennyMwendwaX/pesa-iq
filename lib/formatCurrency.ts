80a89c4a0b07b7a70fa4b6104a4eb3e6ac6603cc
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-09 09:26:44 +0300

    Added funsction formatKESCurrency

--- lib/formatCurrency.ts
+++ lib/formatCurrency.ts
@@ -1,8 +1,17 @@
-const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
+const USD_CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
   currency: "USD",
   style: "currency",
 });
 
-export default function formatCurrency(number: number) {
-  return CURRENCY_FORMATTER.format(number);
+export function formatUSDCurrency(number: number) {
+  return USD_CURRENCY_FORMATTER.format(number);
+}
+
+const KES_CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
+  currency: "KES",
+  style: "currency",
+});
+
+export function formatKESCurrency(number: number) {
+  return USD_CURRENCY_FORMATTER.format(number);
 }
