5fbaab2c85b0a32edaeae5eccf95442fcac6c13b
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-10 09:59:17 +0300

    Added metadata to the layout.tsx of analytis page

--- app/analytics/layout.tsx
+++ app/analytics/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Analytics",
+  description: "Analytics and reports on finances page",
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
