a34d378fd60d705601d5b9843944a74108cf6fd1
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-12 09:04:23 +0300

    Added metadata for layout of /financial-health/page.tsx

--- app/financial-health/layout.tsx
+++ app/financial-health/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Financial Health",
+  description: "Analysis and reports on financial health",
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
