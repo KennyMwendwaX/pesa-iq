397bfb2061575ca4b3b9e5cdee6f79fc9b1b0d74
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-13 08:56:02 +0300

    Added calculateIncomeStabilityScore, calculateSavingsRateScore, calculateDebtToIncomeRatioScore functions

--- app/financial-health/page.tsx
+++ app/financial-health/page.tsx
@@ -1,4 +1,18 @@
-export default function page() {
+"use client";
+
+import { useGetExpenses } from "@/hooks/useGetExpenses";
+import { useGetIncomes } from "@/hooks/useGetIncomes";
+import { calculateFinancialHealthScore } from "@/lib/financialHealthScore";
+
+export default function FinancialHealth() {
+  const { data: incomeData } = useGetIncomes();
+  const { data: expenseData } = useGetExpenses();
+
+  const incomes = incomeData || [];
+  const expenses = expenseData || [];
+
+  calculateFinancialHealthScore({ incomes, expenses });
+
   return (
     <>
       <div className="container mx-auto mt-4 px-12 pb-5 pt-12">
--- lib/financialHealthScore.ts
+++ lib/financialHealthScore.ts
@@ -2,7 +2,7 @@ import { ExpenseTypes } from "@/hooks/useGetExpenses";
 import { IncomeTypes } from "@/hooks/useGetIncomes";
 
 // Calculate average income per month
-export const calculateAverageIncome = (incomes: IncomeTypes[]) => {
+const calculateAverageIncome = (incomes: IncomeTypes[]) => {
   const totalIncome = incomes.reduce(
     (acc, income) => acc + parseFloat(income.amount),
     0
@@ -12,7 +12,7 @@ export const calculateAverageIncome = (incomes: IncomeTypes[]) => {
 };
 
 // Calculate savings rate
-const calculateSavingsRate = async ({
+const calculateSavingsRate = ({
   incomes,
   expenses,
 }: {
@@ -30,4 +30,48 @@ const calculateSavingsRate = async ({
 
   const savingsRate = ((totalIncome - totalExpenses) / totalIncome) * 100;
   return savingsRate;
+};
+
+// Calculate debt-to-income ratio
+const calculateDebtToIncomeRatio = ({
+  incomes,
+  expenses,
+}: {
+  incomes: IncomeTypes[];
+  expenses: ExpenseTypes[];
+}) => {
+  const totalIncome = incomes.reduce(
+    (acc, income) => acc + parseFloat(income.amount),
+    0
+  );
+  const totalDebt = expenses.reduce(
+    (acc, expense) => acc + parseFloat(expense.amount),
+    0
+  );
+
+  const debtToIncomeRatio = (totalDebt / totalIncome) * 100;
+  return debtToIncomeRatio;
+};
+
+// Calculate overall financial health score
+export const calculateFinancialHealthScore = ({
+  incomes,
+  expenses,
+}: {
+  incomes: IncomeTypes[];
+  expenses: ExpenseTypes[];
+}) => {
+  const averageIncome = calculateAverageIncome(incomes);
+  const savingsRate = calculateSavingsRate({ incomes, expenses });
+  const debtToIncomeRatio = calculateDebtToIncomeRatio({ incomes, expenses });
+
+  // You can define your own scoring logic based on these metrics
+  // const incomeStabilityScore = calculateIncomeStabilityScore(averageIncome);
+  // const savingsRateScore = calculateSavingsRateScore(savingsRate);
+  // const debtToIncomeRatioScore = calculateDebtToIncomeRatioScore(debtToIncomeRatio);
+
+  // Calculate overall score based on weights
+  // const overallScore = (incomeStabilityScore * 0.4) + (savingsRateScore * 0.4) + (debtToIncomeRatioScore * 0.2);
+
+  // return overallScore;
 };
