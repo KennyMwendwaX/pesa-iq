57503134cf72efab8110c92b567c0fa805fdfcdb
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-18 07:31:29 +0300

    Added NextAuth authOptions.ts

---
+++ lib/auth/authOptions.ts
@@ -0,0 +1,58 @@
+import type { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/prisma/db";
+import { compare } from "bcrypt";
+
+type MyCredentials = {
+  email: string;
+  password: string;
+};
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: MyCredentials | undefined, req) {
+        // Check if credentials are present
+        if (!credentials) throw new Error("Credentials are required");
+
+        // Check user exists
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user) throw new Error("This email is not registered");
+
+        // Check password
+        const checkPassword =
+          user.password && (await compare(credentials.password, user.password));
+        if (!checkPassword) throw new Error("Email or password doesn't match");
+
+        return {
+          ...user,
+        };
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+};
