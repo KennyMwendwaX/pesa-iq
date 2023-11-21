756c4118a7604b101e42ea9e1a299b1378de9ab5
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-21 10:02:58 +0300

    Added metadata for  layout.tsx of /education-hub

--- app/education-hub/layout.tsx
+++ app/education-hub/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Education Hub",
+  description: "Education hub page",
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
