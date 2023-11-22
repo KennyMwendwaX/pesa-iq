00b98e990c84d08e64b1fb358184436127467a91
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-22 10:28:32 +0300

    Added Layout component for /education-hub/mortgages page

--- app/education-hub/mortgage/layout.tsx
+++ app/education-hub/mortgage/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Mortgages and Articles",
-  description: "Mortgages and related articles",
+  title: "Mortgages and Housing Articles",
+  description: "Mortgages and Housing related articles",
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
